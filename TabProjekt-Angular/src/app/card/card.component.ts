dac0a595a1f0e81a73b9095b5ef026e57cd06d64
Karol Szawara <karosza192@student.polsl.pl>
2022-03-24 18:54:28 +0100

    Przetwarzanie danych miedzy oknami v1

--- TabProjekt-Angular/src/app/card/card.component.ts
+++ TabProjekt-Angular/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Product} from "../core/product";
 
 @Component({
   selector: 'app-card',
@@ -8,21 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   constructor() { }
-  desc='Zainspirowane cyfrowymi technologiami i codziennością. Stworzone dla Twojej satysfakcji. Odblaskowe akcenty podkreślają techniczną cholewkę butów ZX 2K BOOST 2.0. Załóż je i poczuj niewiarygodną miękkość. Amortyzacja Boost daje satysfakcję z każdego kroku.';
-  photos=[{
-    title:'ZX 2K BOOST 2.0 SHOES',
-    url:'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2c43c4cdaf59417b99e4ad2100c3f874_9366/ZX_2K_Boost_2.0_Shoes_Czern_GZ7740_01_standard.jpg',
-    describe:this.desc
-  },
-    {
-      title:'ZX 2K BOOST 2.0 SHOES',
-      url:'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2c43c4cdaf59417b99e4ad2100c3f874_9366/ZX_2K_Boost_2.0_Shoes_Czern_GZ7740_01_standard.jpg',
-      describe:this.desc
-    }
-
-  ]
-
+  @Input() product!:Product ;
+  @Output() eventTask=new EventEmitter<number>();
   ngOnInit(): void {
   }
-
+  select(){
+    this.eventTask.emit(this.product.id);
+  }
 }
