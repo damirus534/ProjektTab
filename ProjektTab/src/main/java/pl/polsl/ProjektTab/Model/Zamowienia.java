c3a92094321e9973afcab5108331914c2b9f2d55
damirus534 <damianrussok@gmail.com>
2022-04-07 22:30:38 +0200

    Some code refactorization

--- ProjektTab/src/main/java/pl/polsl/ProjektTab/Model/Zamowienia.java
+++ ProjektTab/src/main/java/pl/polsl/ProjektTab/Model/Zamowienia.java
@@ -1,8 +1,11 @@
 package pl.polsl.ProjektTab.Model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 public class Zamowienia {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -27,88 +30,4 @@ public class Zamowienia {
     @JoinColumn(name = "id_rekordu_historii", referencedColumnName = "id_rekordu_historii", nullable = false)
     private HistoriaZamowien historiaZamowienByIdRekorduHistorii;
 
-    public int getIdZamownienia() {
-        return idZamownienia;
-    }
-
-    public void setIdZamownienia(int idZamownienia) {
-        this.idZamownienia = idZamownienia;
-    }
-
-    public int getIdProduktu() {
-        return idProduktu;
-    }
-
-    public void setIdProduktu(int idProduktu) {
-        this.idProduktu = idProduktu;
-    }
-
-    public int getIdRekorduHistorii() {
-        return idRekorduHistorii;
-    }
-
-    public void setIdRekorduHistorii(int idRekorduHistorii) {
-        this.idRekorduHistorii = idRekorduHistorii;
-    }
-
-    public int getIloscZakupionych() {
-        return iloscZakupionych;
-    }
-
-    public void setIloscZakupionych(int iloscZakupionych) {
-        this.iloscZakupionych = iloscZakupionych;
-    }
-
-    public double getCenaSprzedazy() {
-        return cenaSprzedazy;
-    }
-
-    public void setCenaSprzedazy(double cenaSprzedazy) {
-        this.cenaSprzedazy = cenaSprzedazy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Zamowienia that = (Zamowienia) o;
-
-        if (idZamownienia != that.idZamownienia) return false;
-        if (idProduktu != that.idProduktu) return false;
-        if (idRekorduHistorii != that.idRekorduHistorii) return false;
-        if (iloscZakupionych != that.iloscZakupionych) return false;
-        if (Double.compare(that.cenaSprzedazy, cenaSprzedazy) != 0) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = idZamownienia;
-        result = 31 * result + idProduktu;
-        result = 31 * result + idRekorduHistorii;
-        result = 31 * result + iloscZakupionych;
-        temp = Double.doubleToLongBits(cenaSprzedazy);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
-    public Produkt getProduktByIdProduktu() {
-        return produktByIdProduktu;
-    }
-
-    public void setProduktByIdProduktu(Produkt produktByIdProduktu) {
-        this.produktByIdProduktu = produktByIdProduktu;
-    }
-
-    public HistoriaZamowien getHistoriaZamowienByIdRekorduHistorii() {
-        return historiaZamowienByIdRekorduHistorii;
-    }
-
-    public void setHistoriaZamowienByIdRekorduHistorii(HistoriaZamowien historiaZamowienByIdRekorduHistorii) {
-        this.historiaZamowienByIdRekorduHistorii = historiaZamowienByIdRekorduHistorii;
-    }
 }
--- ProjektTab/src/main/java/pl/polsl/ProjektTab/ProjektTabApplication.java
+++ ProjektTab/src/main/java/pl/polsl/ProjektTab/ProjektTabApplication.java
@@ -2,11 +2,9 @@ package pl.polsl.ProjektTab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ProjektTabApplication {
 
 	public static void main(String[] args) {
